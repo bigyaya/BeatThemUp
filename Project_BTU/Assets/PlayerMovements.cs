7bb8aa0a72c80698577d0883936393981b1e27a3
nasso <73773951+bigyaya@users.noreply.github.com>
2023-02-06 17:31:52 +0100

    ajout du saut
    le saut du joueur fonctionne

--- Project_BTU/Assets/PlayerMovements.cs
+++ Project_BTU/Assets/PlayerMovements.cs
@@ -6,13 +6,22 @@ public class PlayerMovements : MonoBehaviour
 {
     #region Exposed
 
-    public int runSpeed = 1;
+    Animator animator;
 
+    //run
+    public int runSpeed = 1;
     float horizontal;
     float vertical;
     bool facingRight;
 
-    Animator animator;
+    //jump
+    Rigidbody2D rigidbody1;
+    float axisY;
+    bool isJumping;
+    public float jumpForce = 300;
+
+
+
 
     #endregion
 
@@ -20,37 +29,61 @@ public class PlayerMovements : MonoBehaviour
 
     #region Unity Lifecycle
     void Awake()
-{
+    {
         //cherche le composant des animations (Animator)
         animator = GetComponent<Animator>();
-}
+
+        //cherche le composant rigidbody 
+        rigidbody1 = GetComponent<Rigidbody2D>();
+        rigidbody1.Sleep();
+
+    }
 
     private void Update()
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
 
+        animator.SetFloat("RunSpeed", Mathf.Abs(horizontal != 0 ? horizontal : vertical));
 
-        animator.SetFloat("Speed", Mathf.Abs(horizontal != 0 ? horizontal : vertical));
+
+        //faire sauter le joueur
+
+        if (transform.position.y <= axisY)
+        {
+            OnLanding();
+        }
+
+        if (Input.GetButton("Jump") && !isJumping)
+        {
+            axisY = transform.position.y;
+            isJumping = true;
+            rigidbody1.gravityScale = 1.5f;
+            rigidbody1.WakeUp();
+            rigidbody1.AddForce(new Vector2(transform.position.x + 7.5f, jumpForce));
+            animator.SetBool("IsJumping", isJumping);
+        }
+
+
     }
 
-void FixedUpdate()
-{
+    void FixedUpdate()
+    {
         //création des mouvements static sur les axes
         Vector3 movement = new Vector3(horizontal * runSpeed, vertical * runSpeed, 0.0f);
         transform.position = transform.position + movement * Time.deltaTime;
 
         Flip(horizontal);
-}
+    }
     #endregion
 
 
 
     #region Methods
-    
+
     private void Flip(float horizontal)
     {
-        if (horizontal < 0 && !facingRight || horizontal > 0 && facingRight )
+        if (horizontal < 0 && !facingRight || horizontal > 0 && facingRight)
         {
             facingRight = !facingRight;
 
@@ -58,6 +91,15 @@ void FixedUpdate()
             scale.x *= -1;
             transform.localScale = scale;
         }
+    }
+
+    void OnLanding()
+    {
+        isJumping = false;
+        rigidbody1.gravityScale = 0f;
+        rigidbody1.Sleep();
+        axisY = transform.position.y;
+        animator.SetBool("IsJumping", false);
     }
 
     #endregion
