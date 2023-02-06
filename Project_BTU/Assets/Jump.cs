f28381a128e4b46588ec38fe923897aa871b47f9
Watunip <arnaud.sigala@gmail.com>
2023-02-06 13:54:47 +0100

    le saut du perso

---
+++ Project_BTU/Assets/Jump.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    #region Exposed
+
+    [SerializeField]
+    AnimationCurve _jumpCurve;
+
+    [SerializeField]
+    float _jumpHeight = 3f;
+
+    [SerializeField]
+    float _jumpDuration = 3f;
+    #endregion
+
+    #region Unity Lifecycle
+
+    private void Awake()
+    {
+        _graphics = transform.Find("Graphics");
+    }
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (_jumpTimer < _jumpDuration)
+        {
+            _jumpTimer += Time.deltaTime;
+
+            float y = _jumpCurve.Evaluate(_jumpTimer / _jumpDuration);
+
+            _graphics.localPosition = new Vector3(transform.localPosition.x, y, transform.localPosition.z);
+
+        }
+        else
+        {
+            _jumpTimer = 0f;
+        }
+        
+    }
+    #endregion
+
+    #region Methods
+
+    #endregion
+
+    #region Private & Protected
+
+    Transform _graphics;
+    float _jumpTimer;
+
+    #endregion
+}
