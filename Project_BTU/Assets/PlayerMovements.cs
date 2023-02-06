cc913fb40a502db374168f497b099466ed4a623b
nasso <73773951+bigyaya@users.noreply.github.com>
2023-02-06 16:12:44 +0100

    mouvement du joueur
    le joueur se déplace et se retourne sur l'axe x

--- Project_BTU/Assets/PlayerMovements.cs
+++ Project_BTU/Assets/PlayerMovements.cs
@@ -6,35 +6,59 @@ public class PlayerMovements : MonoBehaviour
 {
     #region Exposed
 
+    public int runSpeed = 1;
+
+    float horizontal;
+    float vertical;
+    bool facingRight;
+
+    Animator animator;
+
     #endregion
 
-	
+
 
     #region Unity Lifecycle
-void Awake()
+    void Awake()
 {
-
+        //cherche le composant des animations (Animator)
+        animator = GetComponent<Animator>();
 }
 
- void Start()
+    private void Update()
     {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
 
-    }
 
- void Update()
-    {
-
+        animator.SetFloat("Speed", Mathf.Abs(horizontal != 0 ? horizontal : vertical));
     }
 
 void FixedUpdate()
 {
+        //création des mouvements static sur les axes
+        Vector3 movement = new Vector3(horizontal * runSpeed, vertical * runSpeed, 0.0f);
+        transform.position = transform.position + movement * Time.deltaTime;
 
+        Flip(horizontal);
 }
     #endregion
 
 
 
     #region Methods
+    
+    private void Flip(float horizontal)
+    {
+        if (horizontal < 0 && !facingRight || horizontal > 0 && facingRight )
+        {
+            facingRight = !facingRight;
+
+            Vector3 scale = transform.localScale;
+            scale.x *= -1;
+            transform.localScale = scale;
+        }
+    }
 
     #endregion
 
