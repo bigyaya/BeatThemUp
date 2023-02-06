0583fb2f7ba28af91c522e667305d57491874d18
nasso <73773951+bigyaya@users.noreply.github.com>
2023-02-06 14:41:46 +0100

    animation du héro
    animations basiques du joueur terminé

---
+++ Project_BTU/Assets/PlayerMovements.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovements : MonoBehaviour
+{
+    #region Exposed
+
+    #endregion
+
+	
+
+    #region Unity Lifecycle
+void Awake()
+{
+
+}
+
+ void Start()
+    {
+
+    }
+
+ void Update()
+    {
+
+    }
+
+void FixedUpdate()
+{
+
+}
+    #endregion
+
+
+
+    #region Methods
+
+    #endregion
+
+
+
+    #region Private & Protected
+
+    #endregion
+}
