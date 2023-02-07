f56dde059f52dd12d60671b4fc12303fabeb9898
nasso <73773951+bigyaya@users.noreply.github.com>
2023-02-07 12:05:38 +0100

    L'ennemie chasse le joueur
    l'ennemie peut chasser le joueur et le cogner à partir d'une certaine distance

---
+++ Project_BTU/Assets/Enemy.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : EnemyBase
+{
+    #region Exposed
+
+    public float speed;
+    public float chaseDistance;
+    public float stopDistance;
+    //le hero
+    public GameObject target;
+
+    private float targetDistance;
+
+    Animator animator;
+
+    #endregion
+
+
+
+    #region Unity Lifecycle
+    void Awake()
+{
+
+}
+
+ void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+ void Update()
+    {
+        targetDistance = Vector2.Distance(transform.position, target.transform.position);
+        if (targetDistance < chaseDistance && targetDistance > stopDistance)
+        {
+            ChasePlayer();
+        }
+        else
+        {
+            stopChasePlayer();
+        }
+    }
+
+   
+
+    void FixedUpdate()
+{
+
+}
+    #endregion
+
+
+
+    #region Methods
+
+    private void ChasePlayer()
+    {
+        if (transform.position.x < target.transform.position.x)
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+
+        }
+        transform.position = Vector2.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+        animator.SetBool("IsWalking", true);
+        animator.SetBool("IsAttacking", false);
+
+    }
+
+    private void stopChasePlayer()
+    {
+        animator.SetBool("IsAttacking", true);
+
+        //ne rien faire
+    }
+
+
+    #endregion
+
+
+
+    #region Private & Protected
+
+    #endregion
+}
---
+++ Project_BTU/Assets/EnemyBase.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBase : MonoBehaviour
+{
+    #region Exposed
+
+    #endregion
+
+	
+
+    #region Unity Lifecycle
+void Awake()
+{
+
+}
+
+ void Start()
+    {
+
+    }
+
+ void Update()
+    {
+
+    }
+
+void FixedUpdate()
+{
+
+}
+    #endregion
+
+
+
+    #region Methods
+
+    #endregion
+
+
+
+    #region Private & Protected
+
+    #endregion
+}
