3a24de9d21a8f1e85af56cfd7cea3e004a3a9a5f
Watunip <arnaud.sigala@gmail.com>
2023-02-13 14:33:14 +0100

    suite de l'ui (2/?)
    suite de l'ui

---
+++ Project_BTU/Assets/Score.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+#region Exposed
+
+    #endregion
+
+    #region Unity Lifecycle
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    #endregion
+
+    #region Methods
+
+    #endregion
+
+    #region Private & Protected
+
+    #endregion
+}
